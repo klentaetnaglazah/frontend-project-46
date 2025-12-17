5ca2b3f13f118a0ef5e0171397d9d086a02f8d26
kawaiirazoru <felixxgg42@gmail.com>
2025-12-17 15:40:04 +0000

    second task

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,16 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing ${filepath1} and ${filepath2}`)
+    console.log(`Output format: ${options.format}`)
+  })
   .helpOption('-h, --help', 'display help for command')
 
-program.parse(process.argv)
+if (process.argv.length <= 2) {
+  program.help()
+} else {
+  program.parse(process.argv)
+}
